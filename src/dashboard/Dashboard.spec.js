ce68961a52af4effbb63407e7ead334ae2e1e3ca
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-14 20:30:00 +0100

    feat: Add snapshot test for the Dashboard component
    In this commit, we import required react testing libraries and take a snapshot test of our <Dashboard /> component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+describe('Dashboard component', () => {
+    it('dashboard component matches the snapshot', () => {
+        expect(wrapper.container).toMatchSnapshot();
+    });
+})
